1929002d6362f4fcfc13913bc33e0cb2a54c57fe
Matias Gonzalez <matiaslg10@gmail.com>
2019-02-04 13:59:46 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -18,8 +18,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:123456b@ds121415.mlab.com:21415/matiaslg10-cafe';
+    urlDB = process.env.MONGO_URL;
 }
-//urlDB = 'mongodb://cafe-user:123456b@ds121415.mlab.com:21415/matiaslg10-cafe';
 
 process.env.URLDB = urlDB;
